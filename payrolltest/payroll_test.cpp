8e88feb056d2ede01e5ad3a2f1846677a2966175
n1428096 <neha.rawat@g.austincc.edu>
2017-09-20 19:11:59 -0500

    payroll

--- payrolltest/payroll_test.cpp
+++ payrolltest/payroll_test.cpp
@@ -1,1 +1,12 @@
+const int NUM_STUDENTS = 3;
+const int NUM_SCORES = 5;
 
+int _tmain(int argc, _TCHAR* ARGV[])
+  
+{
+  DOUBLE SCORES[NUM_STUDENTS][NUM_SCORES] = { {30,50,90 },
+                                             {51,32,35 },
+                                             {62,53,52 } };
+ 
+
+  return 0;
