9f82081391dbe164c91c15250eaede321444a9b5
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2017-09-29 11:49:32 -0500

    incorrect code

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,14 +1,6 @@
 #ifndef _PAYROLL_H
 #define _PAYROLL_H
 
-{
-  double scores[NUM_STUDENTS][NUM_SCORES] = { { 88,97,79,86,94 },
-                                              { 86,85,87,88,89 },
-                                              { 89,53,56,79,96 }};
-  
-   for (int row = 0; row < Num_STUDENTS; row++)
-     total = scores[row]
-
 
 
 #endif
